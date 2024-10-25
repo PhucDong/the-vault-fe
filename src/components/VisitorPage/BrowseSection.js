8bd175abff8460b4664727912acbc08a487688ca
Phuc <dongvuminhphuc@gmail.com>
2024-10-25 15:15:58 +0700

    Removed some comments

--- src/components/VisitorPage/BrowseSection.js
+++ src/components/VisitorPage/BrowseSection.js
@@ -1,17 +1,12 @@
 import { Box } from "@mui/material";
 import BrowseCategory from "./BrowseCategory";
 import SearchBar from "./SearchBar";
-// import { useLocation } from "react-router-dom";
 
 function BrowseSection() {
-  // const location = useLocation();
-
   return (
     <Box
       sx={{
-        // padding: location.pathname === "/" ? "0 22px" : 0,
         padding: "0 22px",
-        // marginBottom: location.pathname !== "/" ? "52px" : 0,
       }}
     >
       <BrowseCategory />
