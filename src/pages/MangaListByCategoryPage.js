fdc32bdbc323cea0d135a81719e484d1f381d052
Phuc <dongvuminhphuc@gmail.com>
2024-10-25 15:18:37 +0700

    Added MangaListByCategoryPage

---
+++ src/pages/MangaListByCategoryPage.js
@@ -0,0 +1,38 @@
+import { Box } from "@mui/material";
+import AnimeCategory from "../components/VisitorPage/AnimeCategory";
+import { useLoaderData, useParams } from "react-router-dom";
+import { useCallback } from "react";
+
+function MangaListByCategoryPage() {
+  const { categoryName } = useParams();
+  const mangaCategoryList = useLoaderData();
+
+  const getMangaCategoryList = useCallback(() => {
+    const allowedMangaCategoryList = ["trending", "popular", "top-100-mangas"];
+    if (categoryName && allowedMangaCategoryList.includes(categoryName)) {
+      const filteredAnimeCategory = mangaCategoryList.filter(
+        (category) => category.category === categoryName
+      );
+
+      return filteredAnimeCategory;
+    } else {
+      throw new Error(`Category ${categoryName} doesn't exist`);
+    }
+  }, [categoryName, mangaCategoryList]);
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "40px",
+      }}
+    >
+      {getMangaCategoryList().map((animeCategory, index) => (
+        <AnimeCategory key={index} animeCategory={animeCategory} />
+      ))}
+    </Box>
+  );
+}
+
+export default MangaListByCategoryPage;
