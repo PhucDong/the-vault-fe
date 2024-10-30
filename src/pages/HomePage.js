24e1cac625fa483317162fda01bfec01fe2908f1
Phuc <dongvuminhphuc@gmail.com>
2024-10-31 00:18:26 +0700

    Added home page after register/login

---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+function HomePage() {
+  return (
+    <Box>
+      <Typography>Welcome to The Vault</Typography>
+    </Box>
+  );
+}
+
+export default HomePage;
