d122c7150d4cf61e33a627c50f878929c3c9ed28
Phuc <dongvuminhphuc@gmail.com>
2024-10-31 00:19:09 +0700

    Created register page

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,10 +1,33 @@
 import { Box, Typography } from "@mui/material";
-import React from "react";
+import { NavLink } from "react-router-dom";
+import RegisterForm from "../components/RegisterPage/RegisterForm";
 
 function RegisterPage() {
   return (
     <Box>
-      <Typography>This is a register page</Typography>
+      <Typography
+        sx={{
+          fontSize: "1.5rem",
+          fontWeight: 600,
+          textAlign: "center",
+          marginBottom: "32px",
+        }}
+      >
+        Register to The Vault
+      </Typography>
+
+      <RegisterForm />
+
+      <Typography
+        sx={{
+          marginTop: "16px",
+          textAlign: "center",
+          fontSize: "0.875rem",
+          "& a": { color: "primary.main", fontWeight: 600 },
+        }}
+      >
+        Already have an account? <NavLink to="/auth/login">Log in</NavLink>
+      </Typography>
     </Box>
   );
 }
