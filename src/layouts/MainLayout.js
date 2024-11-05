3ca9f6381e3ad821d7de17e08c9b9ebb9483c24d
Phuc <dongvuminhphuc@gmail.com>
2024-11-05 15:38:22 +0700

    Created

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -2,22 +2,35 @@ import { Box } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import MainHeader from "../components/common/MainHeader";
 import MainFooter from "../components/common/MainFooter";
+import { useState } from "react";
 
 function MainLayout() {
+  const [navHeight, setNavHeight] = useState(0);
+
   return (
     <Box
       sx={{
         display: "flex",
         flexDirection: "column",
-        height: "100vh",
+        minHeight: "100vh",
         width: "100%",
         justifyContent: "space-between",
-        gap: "52px",
       }}
     >
-      <MainHeader />
-      <Outlet />
-      <MainFooter />
+      <MainHeader setNavHeight={setNavHeight} />
+      <Box
+        sx={{
+          flex: 1,
+          marginTop: { xs: 0, md: `${navHeight}px` },
+          marginBottom: { xs: "52px", md: "60px", lg: "68px" },
+          display: "flex",
+          flexDirection: "column",
+          gap: { xs: "52px", md: "60px", lg: "68px" },
+        }}
+      >
+        <Outlet />
+      </Box>
+      <MainFooter navHeight={navHeight} />
     </Box>
   );
 }
