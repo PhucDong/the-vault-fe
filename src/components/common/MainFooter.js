71190ecca17912c267dfd145782147deab33e1c2
Phuc <dongvuminhphuc@gmail.com>
2024-11-05 14:48:43 +0700

    Added Footer component

--- src/components/common/MainFooter.js
+++ src/components/common/MainFooter.js
@@ -1,38 +1,70 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { NavLink } from "react-router-dom";
 
-function MainFooter() {
+function MainFooter({ navHeight }) {
   return (
     <Box
       sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        gap: "20px",
-        padding: "32px 32px 96px 32px",
-        backgroundColor: "primary.main",
-        "& a": {
-          textAlign: "center",
-          color: "secondary.main",
-          textDecoration: "none",
+        padding: {
+          xs: "32px 0",
+          sm: "32px 0",
+          md: "32px 60px",
+          lg: "32px 140px",
         },
+        marginBottom: { xs: `${navHeight}px`, md: 0 },
+        backgroundColor: "primary.main",
       }}
     >
-      <NavLink to="/" style={{ fontSize: "1.8rem", fontWeight: 600 }}>
-        The Vault
-      </NavLink>
       <Box
         sx={{
           display: "flex",
-          gap: "20px",
+          flexDirection: { xs: "column", md: "row" },
+          alignItems: "center",
+          gap: { xs: "20px", md: 0 },
           "& a": {
-            fontSize: "1.25rem",
+            textAlign: "center",
+            color: "secondary.main",
+            textDecoration: "none",
+            lineHeight: "100%",
           },
         }}
       >
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="auth/login">Log In</NavLink>
-        <NavLink to="users">Register</NavLink>
+        <Box
+          component={NavLink}
+          to="/"
+          sx={{
+            fontSize: { xs: "2rem", md: "2.2rem", lg: "2.4rem" },
+            fontWeight: 550,
+            marginRight: { md: "140px", lg: "160px" },
+          }}
+        >
+          The Vault
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            gap: { xs: "20px", md: "32px" },
+            "& a": {
+              fontSize: { xs: "1.25rem", md: "1.4rem" },
+            },
+          }}
+        >
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="auth/login">Log In</NavLink>
+          <NavLink to="users">Register</NavLink>
+        </Box>
+      </Box>
+      <Box sx={{ marginTop: { xs: "20px", md: "40px" } }}>
+        <Typography
+          sx={{
+            color: "#fff",
+            textAlign: { xs: "center", md: "left" },
+            fontSize: "0.8rem",
+          }}
+        >
+          Copyright &copy; 2024 DVMP. All rights reserved.
+        </Typography>
       </Box>
     </Box>
   );
