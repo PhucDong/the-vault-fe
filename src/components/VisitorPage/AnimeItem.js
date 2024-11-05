80be9936dd7636159a2ab61ca141020d0af572aa
Phuc <dongvuminhphuc@gmail.com>
2024-11-05 15:30:04 +0700

    Added a

--- src/components/VisitorPage/AnimeItem.js
+++ src/components/VisitorPage/AnimeItem.js
@@ -2,19 +2,31 @@ import { Box, Typography } from "@mui/material";
 
 function AnimeItem({ anime }) {
   return (
-    <Box sx={{ flex: 1 }}>
+    <Box
+      sx={{
+        width: "100%", // Takes full width of the Grid item
+      }}
+    >
+      {/* Item image */}
       <Box
         sx={{
-          width: "110px",
-          height: "174px",
+          width: "100%",
+          aspectRatio: "5 / 6.5", // Maintains a consistent aspect ratio
           backgroundColor: "#ABABAB",
           marginBottom: "6px",
         }}
       ></Box>
 
-      <Box>
+      {/* Item title */}
+      <Box sx={{ maxWidth: "100%" }}>
         <Typography
-          sx={{ fontSize: "14px", fontWeight: 600, lineHeight: 1.25 }}
+          sx={{
+            fontSize: { xs: "0.875rem", md: "0.975rem", lg: "1rem" },
+            fontWeight: 600,
+            lineHeight: 1.25,
+            color: "primary.dark",
+            textOverflow: "ellipsis", // Prevents long titles from breaking layout
+          }}
         >
           {anime.title}
         </Typography>
