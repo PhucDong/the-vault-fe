5ed600404e4764f42799d927991e97cf358ff2ee
Phuc <dongvuminhphuc@gmail.com>
2024-11-05 15:45:14 +0700

    Added a list of mangas

--- src/pages/MangaListPage.js
+++ src/pages/MangaListPage.js
@@ -1,20 +1,16 @@
 import { Box } from "@mui/material";
 import AnimeCategory from "../components/VisitorPage/AnimeCategory";
-import { useLoaderData, useLocation } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 
 function MangaListPage() {
   const mangaCategoryList = useLoaderData();
-  const location = useLocation();
-
-  console.log("Manga list page!");
 
   return (
     <Box
       sx={{
-        padding: location.pathname === "/" ? "0 22px" : 0,
         display: "flex",
         flexDirection: "column",
-        gap: "40px",
+        gap: { xs: "48px", md: "60px" },
       }}
     >
       {mangaCategoryList.map((animeCategory, index) => (
