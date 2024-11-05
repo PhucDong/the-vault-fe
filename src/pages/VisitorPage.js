06d662dd00727828f01a54119fe6052b7b8d5518
Phuc <dongvuminhphuc@gmail.com>
2024-11-05 15:45:28 +0700

    Added visitor page

--- src/pages/VisitorPage.js
+++ src/pages/VisitorPage.js
@@ -2,19 +2,27 @@ import HeroSection from "../components/VisitorPage/HeroSection";
 import BrowseSection from "../components/VisitorPage/BrowseSection";
 import AnimeListPage from "./AnimeListPage";
 import { Outlet, useLocation } from "react-router-dom";
+import { Box } from "@mui/material";
 
 function VisitorPage() {
   const location = useLocation();
 
-  console.log("Visitor page!");
-  console.log("Location: ", location);
-
   return (
     <>
       {location.pathname === "/" && <HeroSection />}
-      <BrowseSection />
-      {location.pathname === "/" && <AnimeListPage />}
-      <Outlet />
+      <Box
+        sx={{
+          padding: {
+            xs: "0 22px",
+            md: "0 60px",
+            lg: "0 80px",
+          },
+        }}
+      >
+        <BrowseSection />
+        {location.pathname === "/" && <AnimeListPage />}
+        <Outlet />
+      </Box>
     </>
   );
 }
