9ad0742992f7d14a720c6f23693fcb98325e2a18
Phuc <dongvuminhphuc@gmail.com>
2024-11-05 15:36:56 +0700

    Added the search bar component

--- src/components/VisitorPage/SearchBar.js
+++ src/components/VisitorPage/SearchBar.js
@@ -42,53 +42,61 @@ function SearchBar() {
   };
 
   return (
-    <Box>
-      <Autocomplete
-        value={selectedOption}
-        onChange={handleChangeSelectedOption}
-        inputValue={searchValue}
-        onInputChange={handleChangeSearchValue}
-        onKeyDown={handleEnterSearchValue}
-        freeSolo
-        options={searchHistoryList.map((option) => option)}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            placeholder="Search for animes"
-            slotProps={{
-              input: {
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <SearchIcon />
-                  </InputAdornment>
-                ),
+    <>
+      <Box
+        sx={{ display: { xs: "block", sm: "flex" }, gap: { xs: 0, sm: "4px" } }}
+      >
+        <Autocomplete
+          value={selectedOption}
+          onChange={handleChangeSelectedOption}
+          inputValue={searchValue}
+          onInputChange={handleChangeSearchValue}
+          onKeyDown={handleEnterSearchValue}
+          freeSolo
+          options={searchHistoryList.map((option) => option)}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              placeholder="Search for animes"
+              slotProps={{
+                input: {
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <SearchIcon />
+                    </InputAdornment>
+                  ),
+                },
+              }}
+            />
+          )}
+          sx={{
+            width: "100%",
+            marginBottom: { xs: "4px", sm: 0 },
+            "& .MuiOutlinedInput-root": {
+              borderRadius: "12px",
+              padding: { xs: "4px 12px", sm: "6px 14px" },
+              height: { xs: "42px", sm: "46px", md: "50px" },
+              "& .MuiSvgIcon-root": {
+                color: "primary.light",
+                fontSize: { xs: "1.6em", md: "1.8rem" },
               },
-            }}
-          />
-        )}
-        sx={{
-          width: "100%",
-          marginBottom: "4px",
-          "& .MuiOutlinedInput-root": {
-            borderRadius: "12px",
-            padding: "4px 12px",
-            height: "42px",
-            "& .MuiSvgIcon-root": { color: "primary.light" },
-            "& .MuiAutocomplete-input": {
-              padding: 0,
-              color: "primary.light",
+              "& .MuiAutocomplete-input": {
+                padding: 0,
+                color: "primary.light",
+              },
+              "& .MuiOutlinedInput-input": {
+                lineHeight: "100%",
+                fontSize: { xs: "1.1rem", sm: "1.2rem", md: "1.3rem" },
+              },
             },
-            "& .MuiOutlinedInput-input": {
-              lineHeight: "100%",
-            },
-          },
-        }}
-      />
-      <CustomStyledSearchButton onClick={handleSearchAnimeManga}>
-        Search
-      </CustomStyledSearchButton>
+          }}
+        />
+        <CustomStyledSearchButton onClick={handleSearchAnimeManga}>
+          Search
+        </CustomStyledSearchButton>
+      </Box>
       <AdvancedSearch />
-    </Box>
+    </>
   );
 }
 
