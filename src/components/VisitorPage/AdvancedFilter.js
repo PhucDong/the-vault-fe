afb3e9ce1a6095ff5e75863cb1034a82edea5cdc
Phuc <dongvuminhphuc@gmail.com>
2024-11-05 15:24:16 +0700

    Created a component for each advanced filter

--- src/components/VisitorPage/AdvancedFilter.js
+++ src/components/VisitorPage/AdvancedFilter.js
@@ -8,16 +8,21 @@ import {
 } from "@mui/material";
 import { useState } from "react";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs from "dayjs";
 
 function AdvancedFilter({ advancedFilter }) {
-  const [advancedFilterOption, setAdvancedFilterOption] = useState(
-    advancedFilter.filterName !== "Year" &&
-      advancedFilter.filterName !== "Airing Status"
-      ? []
-      : ""
-  );
+  const [advancedFilterOption, setAdvancedFilterOption] = useState(() => {
+    if (advancedFilter.filterName === "Airing Status") {
+      return "";
+    } else if (advancedFilter.filterName === "Year") {
+      return dayjs();
+    } else {
+      return [];
+    }
+  });
 
-  // console.log("Advanced filter option: ", advancedFilterOption);
   const [openAdvancedFilterDropdownMenu, setOpenAdvancedFilterDropdownMenu] =
     useState(false);
 
@@ -33,118 +38,176 @@ function AdvancedFilter({ advancedFilter }) {
     setOpenAdvancedFilterDropdownMenu(false);
   };
 
+  const filterId = `${advancedFilter.filterName}`;
+
   return (
-    <Box>
+    <Box sx={{ width: { sm: "49.7%", lg: "24.5%" } }}>
       <InputLabel
-        id={`${advancedFilter.filterName}-label`}
-        // className="section-label"
+        htmlFor={`${advancedFilter.filterName}`}
         sx={{
           color: "#70787a",
           fontWeight: 600,
-          fontSize: "0.875rem",
-          marginBottom: "6px",
+          fontSize: { xs: "0.875rem", md: "1rem" },
+          marginBottom: "4px",
         }}
       >
         {advancedFilter.filterName}
       </InputLabel>
-      <TextField
-        name={`${advancedFilter.filterName}`}
-        // className="section-input"
-        required
-        select
-        hiddenLabel
-        value={advancedFilterOption}
-        onChange={handleChangeAdvancedFilterOption}
-        slotProps={{
-          select: {
-            multiple:
-              advancedFilter.filterName !== "Year" &&
-              advancedFilter.filterName !== "Airing Status"
-                ? true
-                : false,
-            renderValue: (selected) =>
-              advancedFilter.filterName !== "Year" &&
-              advancedFilter.filterName !== "Airing Status"
-                ? selected.map((item) => item).join(", ")
-                : selected,
-            open: openAdvancedFilterDropdownMenu,
-            onOpen: handleOpenAdvancedFilterDropdownMenu,
-            onClose: handleCloseAdvancedFilterDropdownMenu,
-            IconComponent: KeyboardArrowRightIcon,
-            MenuProps: {
-              PaperProps: {
-                sx: {
-                  padding: "8px 0",
-                  boxShadow: "0 2px 4px 0 rgba(0, 0, 0, 0.2)",
+      {advancedFilter.filterName === "Year" ? (
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePicker
+            views={["year"]}
+            yearsOrder="desc"
+            maxDate={dayjs()}
+            value={advancedFilterOption}
+            onChange={(newYearValue) => setAdvancedFilterOption(newYearValue)}
+            slots={{ openPickerIcon: KeyboardArrowRightIcon }}
+            onOpen={handleOpenAdvancedFilterDropdownMenu}
+            onClose={handleCloseAdvancedFilterDropdownMenu}
+            slotProps={{
+              textField: {
+                id: `${advancedFilter.filterName}`,
+                name: filterId,
+                fullWidth: true,
+                placeholder: "",
+                InputProps: {
+                  sx: {
+                    "& .MuiSvgIcon-root": {
+                      fontSize: "1.75rem",
+                      transform: openAdvancedFilterDropdownMenu
+                        ? "rotate(90deg)"
+                        : "none",
+                    },
+                  },
                 },
               },
-              sx: {
-                "& .MuiMenu-paper": {
-                  padding: 0,
-                },
-                "& .MuiMenu-list": {
-                  padding: 0,
-                  "& .MuiMenuItem-root": {
-                    padding: "8px 20px",
+            }}
+            sx={{
+              position: "relative",
+              width: "100%",
+              "& .MuiOutlinedInput-input": {
+                padding: 0,
+              },
+              "& .MuiOutlinedInput-root": {
+                display: "flex",
+                padding: "8px 16px",
+                justifyContent: "space-between",
+                alignItems: "center",
+                gap: "4px",
+                backgroundColor: "#E9E9E9",
+                borderRadius: "8px",
+                "& .MuiOutlinedInput-notchedOutline": { border: "none" },
+                "& .MuiInputBase-input": {
+                  "&.MuiOutlinedInput-input": {
+                    fontSize: { xs: "1.2rem", sm: "1.1rem", lg: "1rem" },
+                    padding: 0,
                     minHeight: "100%",
-                    gap: "4px",
-                    "& .MuiCheckbox-root": {
-                      padding: 0,
-                    },
-                    "& .MuiTypography-root": {
-                      fontSize: "0.95rem",
-                      fontWeight: 550,
-                    },
-                    "&.Mui-selected": {
-                      color: "info.main",
+                    height: "100%",
+                  },
+                },
+              },
+              "& .MuiIconButton-root": {
+                padding: 0,
+                margin: 0,
+              },
+            }}
+          />
+        </LocalizationProvider>
+      ) : (
+        <TextField
+          id={`${advancedFilter.filterName}`}
+          required
+          select
+          hiddenLabel
+          value={advancedFilterOption}
+          onChange={handleChangeAdvancedFilterOption}
+          slotProps={{
+            select: {
+              multiple:
+                advancedFilter.filterName !== "Airing Status" ? true : false,
+              renderValue: (selected) =>
+                advancedFilter.filterName !== "Airing Status"
+                  ? selected.map((item) => item).join(", ")
+                  : selected,
+              open: openAdvancedFilterDropdownMenu,
+              onOpen: handleOpenAdvancedFilterDropdownMenu,
+              onClose: handleCloseAdvancedFilterDropdownMenu,
+              IconComponent: KeyboardArrowRightIcon,
+              MenuProps: {
+                PaperProps: {
+                  sx: {
+                    padding: "8px 0",
+                    boxShadow: "0 2px 4px 0 rgba(0, 0, 0, 0.2)",
+                  },
+                },
+                sx: {
+                  "& .MuiMenu-paper": {
+                    padding: 0,
+                  },
+                  "& .MuiMenu-list": {
+                    padding: 0,
+                    "& .MuiMenuItem-root": {
+                      padding: "8px 20px",
+                      minHeight: "100%",
+                      gap: "4px",
+                      "& .MuiCheckbox-root": {
+                        padding: 0,
+                      },
+                      "& .MuiTypography-root": {
+                        fontSize: "0.95rem",
+                        fontWeight: 550,
+                      },
+                      "&.Mui-selected": {
+                        color: "info.main",
+                      },
                     },
                   },
                 },
               },
             },
-          },
-        }}
-        sx={{
-          width: "100%",
-          "& .MuiOutlinedInput-input": {
-            padding: 0,
-          },
-          "& .MuiOutlinedInput-root": {
-            display: "flex",
-            padding: "8px 16px",
-            justifyContent: "space-between",
-            alignItems: "center",
-            gap: "4px",
-            backgroundColor: "#E9E9E9",
-            borderRadius: "8px",
-            "& .MuiOutlinedInput-notchedOutline": { border: "none" },
-            "& .MuiInputBase-input": {
-              "&.MuiOutlinedInput-input": {
-                fontSize: "1.2rem",
-                padding: 0,
-                minHeight: "100%",
-                height: "100%",
+          }}
+          sx={{
+            width: "100%",
+            "& .MuiOutlinedInput-input": {
+              padding: 0,
+            },
+            "& .MuiOutlinedInput-root": {
+              display: "flex",
+              padding: "8px 16px",
+              justifyContent: "space-between",
+              alignItems: "center",
+              gap: "4px",
+              backgroundColor: "#E9E9E9",
+              borderRadius: "8px",
+              "& .MuiOutlinedInput-notchedOutline": { border: "none" },
+              "& .MuiInputBase-input": {
+                "&.MuiOutlinedInput-input": {
+                  fontSize: { xs: "1.2rem", sm: "1.1rem", lg: "1rem" },
+                  padding: 0,
+                  minHeight: "100%",
+                  height: "100%",
+                },
               },
             },
-          },
-          "& .MuiSvgIcon-root": {
-            position: "initial",
-            fontSize: "1.75rem",
-            transform: openAdvancedFilterDropdownMenu && "rotate(90deg)",
-          },
-        }}
-      >
-        {advancedFilter.options.map((option) => (
-          <MenuItem key={option} value={option}>
-            <Checkbox
-              id={option}
-              name={option}
-              checked={advancedFilterOption.indexOf(option) > -1}
-            />
-            <ListItemText primary={option} />
-          </MenuItem>
-        ))}
-      </TextField>
+            "& .MuiSvgIcon-root": {
+              position: "initial",
+              fontSize: "1.75rem",
+              transform: openAdvancedFilterDropdownMenu && "rotate(90deg)",
+            },
+          }}
+        >
+          {advancedFilter.options.map((option) => (
+            <MenuItem key={option} value={option}>
+              <Checkbox
+                id={option}
+                name={option}
+                checked={advancedFilterOption.indexOf(option) > -1}
+              />
+              <ListItemText primary={option} />
+            </MenuItem>
+          ))}
+        </TextField>
+      )}
     </Box>
   );
 }
