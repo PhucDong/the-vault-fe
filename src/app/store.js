d295797a0c7614a8b8712305fa4b2267d731d987
Phuc <dongvuminhphuc@gmail.com>
2024-12-18 00:33:01 +0700

    Add anime & manga reducers to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/user/userSlice";
 import authenticationReducer from "../features/authentication/authenticationSlice";
+import animeReducer from "../features/anime/animeSlice";
+import mangaReducer from "../features/manga/mangaSlice";
 
 const rootReducer = {
   authentication: authenticationReducer,
   user: userReducer,
+  anime: animeReducer,
+  manga: mangaReducer,
 };
 
 const store = configureStore({
