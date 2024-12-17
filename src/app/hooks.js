ff825420170d5091c177f1d82b3b8097c80f4946
Phuc <dongvuminhphuc@gmail.com>
2024-12-18 00:32:44 +0700

    Add custom hooks for animes and mangas

--- src/app/hooks.js
+++ src/app/hooks.js
@@ -3,6 +3,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { bindActionCreators } from "@reduxjs/toolkit";
 import * as authenticationSliceActions from "../features/authentication/authenticationSlice";
 import * as userSliceActions from "../features/user/userSlice";
+import * as animeSliceActions from "../features/anime/animeSlice";
+import * as mangaSliceActions from "../features/manga/mangaSlice";
 
 // Custom useAppSelector hook
 export const useAppSelector = (selector) => useSelector(selector);
@@ -16,4 +18,14 @@ export const useAuthAppDispatch = () => {
 export const useUserAppDispatch = () => {
   const dispatch = useDispatch();
   return bindActionCreators(userSliceActions, dispatch);
+};
+
+export const useAnimeAppDispatch = () => {
+  const dispatch = useDispatch();
+  return bindActionCreators(animeSliceActions, dispatch);
+};
+
+export const useMangaAppDispatch = () => {
+  const dispatch = useDispatch();
+  return bindActionCreators(mangaSliceActions, dispatch);
 };
