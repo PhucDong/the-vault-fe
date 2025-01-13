cc6b7e747217f233ec93f52ac682bdb857bb4c72
Phuc <dongvuminhphuc@gmail.com>
2025-01-13 23:56:58 +0700

    Created filter for mangas

---
+++ src/components/MangaSearchPage/MangaStatusFilter.js
@@ -0,0 +1,174 @@
+import {
+  Box,
+  Checkbox,
+  InputLabel,
+  ListItemText,
+  MenuItem,
+  TextField,
+} from "@mui/material";
+import { useState } from "react";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import { useMangaAppDispatch } from "../../services/hooks";
+
+function MangaStatusFilter(props) {
+  const { statusOption, sx } = props;
+  const { updatePublishingStatus } = useMangaAppDispatch();
+
+  const handleChangeAiringStatusFilter = (option) => {
+    if (statusOption === option) {
+      updatePublishingStatus("");
+    } else {
+      updatePublishingStatus(option);
+    }
+  };
+
+  const [openAdvancedFilterDropdownMenu, setOpenAdvancedFilterDropdownMenu] =
+    useState(false);
+
+  const handleOpenAdvancedFilterDropdownMenu = () => {
+    setOpenAdvancedFilterDropdownMenu(true);
+  };
+
+  const handleCloseAdvancedFilterDropdownMenu = () => {
+    setOpenAdvancedFilterDropdownMenu(false);
+  };
+
+  return (
+    <Box sx={sx}>
+      <InputLabel
+        htmlFor="publishing-status-filter"
+        sx={{
+          color: "#70787a",
+          fontWeight: 600,
+          fontSize: { xs: "0.875rem", md: "1rem" },
+          marginBottom: "4px",
+        }}
+      >
+        Publishing Status
+      </InputLabel>
+      <TextField
+        id="publishing-status-filter"
+        required
+        select
+        hiddenLabel
+        value={statusOption}
+        slotProps={{
+          select: {
+            multiple: false,
+            renderValue: (selected) => selected,
+            open: openAdvancedFilterDropdownMenu,
+            onOpen: handleOpenAdvancedFilterDropdownMenu,
+            onClose: handleCloseAdvancedFilterDropdownMenu,
+            IconComponent: KeyboardArrowRightIcon,
+            MenuProps: {
+              PaperProps: {
+                sx: {
+                  padding: "8px 0",
+                  boxShadow: "0 2px 4px 0 rgba(0, 0, 0, 0.2)",
+                },
+              },
+              sx: {
+                "& .MuiMenu-paper": {
+                  padding: 0,
+                },
+                "& .MuiMenu-list": {
+                  padding: 0,
+                  "& .MuiMenuItem-root": {
+                    padding: "8px 20px",
+                    minHeight: "100%",
+                    gap: "4px",
+                    "& .MuiCheckbox-root": {
+                      padding: 0,
+                    },
+                    "& .MuiTypography-root": {
+                      fontSize: "0.95rem",
+                      fontWeight: 550,
+                    },
+                    "&.Mui-selected": {
+                      color: "info.main",
+                    },
+                  },
+                },
+              },
+            },
+          },
+          textField: {
+            id: "airing-status-filter",
+            name: "airing-status-filter",
+            fullWidth: true,
+            placeholder: "",
+            InputProps: {
+              onClick: handleOpenAdvancedFilterDropdownMenu,
+              sx: {
+                cursor: "pointer",
+              },
+            },
+          },
+        }}
+        sx={{
+          width: "100%",
+          "& .MuiOutlinedInput-input": {
+            padding: 0,
+          },
+          "& .MuiOutlinedInput-root": {
+            cursor: "pointer",
+            display: "flex",
+            padding: "8px 16px",
+            justifyContent: "space-between",
+            alignItems: "center",
+            gap: "4px",
+            backgroundColor: "#E9E9E9",
+            borderRadius: "8px",
+            "& .MuiOutlinedInput-notchedOutline": { border: "none" },
+            "& .MuiInputBase-input": {
+              "&.MuiOutlinedInput-input": {
+                cursor: "inherit",
+                fontSize: { xs: "1.2rem", sm: "1.1rem", lg: "1rem" },
+                padding: 0,
+                minHeight: "100%",
+                height: "100%",
+                textTransform: "capitalize",
+              },
+            },
+          },
+          "& .MuiSvgIcon-root": {
+            position: "initial",
+            fontSize: "1.75rem",
+            transform: openAdvancedFilterDropdownMenu && "rotate(90deg)",
+          },
+        }}
+      >
+        {[
+          "releasing",
+          "finished",
+          "not yet released",
+          "hiatus",
+          "cancelled",
+        ].map((option) => (
+          <MenuItem
+            key={option}
+            value={option}
+            onClick={() => handleChangeAiringStatusFilter(option)}
+          >
+            <Checkbox
+              id={option}
+              name={option}
+              checked={statusOption === option}
+            />
+            <ListItemText
+              primary={option}
+              sx={{
+                "& .MuiTypography-root": {
+                  color: "primary.main",
+                  textTransform: "capitalize",
+                },
+              }}
+            />
+          </MenuItem>
+        ))}
+      </TextField>
+    </Box>
+  );
+}
+
+export default MangaStatusFilter;
---
+++ src/components/MangaSearchPage/MangaYearFilter.js
@@ -0,0 +1,115 @@
+import { Box, InputLabel } from "@mui/material";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import dayjs from "dayjs";
+import { useState } from "react";
+import { useMangaAppDispatch } from "../../services/hooks";
+
+function MangaYearFilter(props) {
+  const { yearOption, sx } = props;
+  const { updatePublishedYear } = useMangaAppDispatch();
+  const [clearedYearOption, setClearedYearOption] = useState(false);
+
+  const handleChangeYearFilter = (newYear) => {
+    updatePublishedYear(newYear?.toISOString());
+  };
+
+  const [openAdvancedFilterDropdownMenu, setOpenAdvancedFilterDropdownMenu] =
+    useState(false);
+
+  const handleOpenAdvancedFilterDropdownMenu = () => {
+    setOpenAdvancedFilterDropdownMenu(true);
+  };
+
+  const handleCloseAdvancedFilterDropdownMenu = () => {
+    setOpenAdvancedFilterDropdownMenu(false);
+  };
+
+  return (
+    <Box sx={sx}>
+      <InputLabel
+        htmlFor="year-filter"
+        sx={{
+          color: "#70787a",
+          fontWeight: 600,
+          fontSize: { xs: "0.875rem", md: "1rem" },
+          marginBottom: "4px",
+        }}
+      >
+        Year
+      </InputLabel>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DatePicker
+          views={["year"]}
+          yearsOrder="desc"
+          maxDate={dayjs()}
+          // value={dayjs(yearOption)}
+          value={yearOption ? dayjs(yearOption) : null}
+          onChange={handleChangeYearFilter}
+          slots={{ openPickerIcon: KeyboardArrowRightIcon }}
+          open={openAdvancedFilterDropdownMenu} // Controls the dropdown's visibility
+          onOpen={handleOpenAdvancedFilterDropdownMenu}
+          onClose={handleCloseAdvancedFilterDropdownMenu}
+          slotProps={{
+            textField: {
+              id: "year-filter",
+              name: "year-filter",
+              fullWidth: true,
+              placeholder: "",
+              InputProps: {
+                onClick: handleOpenAdvancedFilterDropdownMenu,
+                sx: {
+                  cursor: "pointer",
+                  "& .MuiSvgIcon-root": {
+                    fontSize: "1.75rem",
+                    transform: openAdvancedFilterDropdownMenu
+                      ? "rotate(90deg)"
+                      : "none",
+                  },
+                },
+              },
+            },
+            field: {
+              clearable: true,
+              onClear: () => setClearedYearOption(true),
+            },
+          }}
+          sx={{
+            position: "relative",
+            width: "100%",
+            "& .MuiOutlinedInput-input": {
+              padding: 0,
+            },
+            "& .MuiOutlinedInput-root": {
+              cursor: "pointer",
+              display: "flex",
+              padding: "8px 16px",
+              justifyContent: "space-between",
+              alignItems: "center",
+              gap: "4px",
+              backgroundColor: "#E9E9E9",
+              borderRadius: "8px",
+              "& .MuiOutlinedInput-notchedOutline": { border: "none" },
+              "& .MuiInputBase-input": {
+                "&.MuiOutlinedInput-input": {
+                  cursor: "inherit", // Inherits pointer cursor for consistency
+                  fontSize: { xs: "1.2rem", sm: "1.1rem", lg: "1rem" },
+                  padding: 0,
+                  minHeight: "100%",
+                  height: "100%",
+                },
+              },
+            },
+            "& .MuiIconButton-root": {
+              padding: 0,
+              margin: 0,
+            },
+          }}
+        />
+      </LocalizationProvider>
+    </Box>
+  );
+}
+
+export default MangaYearFilter;
