557f8e6cedda439940e33879f5b82feeaff3aef6
Phuc <dongvuminhphuc@gmail.com>
2025-01-21 12:30:01 +0700

    Persisted specific states in slices

--- src/store/store.js
+++ src/store/store.js
@@ -8,6 +8,7 @@ import {
   PERSIST,
   PURGE,
   REGISTER,
+  createTransform,
 } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // default localStorage for web
 import userReducer from "./slices/user/userSlice";
@@ -17,12 +18,54 @@ import mangaReducer from "./slices/manga/mangaSlice";
 import autoMergeLevel2 from "redux-persist/es/stateReconciler/autoMergeLevel2";
 import { isSessionActive } from "../utils/sessionUtils";
 
+// Custom transform to persist some states in userSlice
+const userTransform = createTransform(
+  // Transform function to filter data when saving
+  (inboundState, key) => {
+    return {
+      isUserRegistered: inboundState.isUserRegistered,
+      email: inboundState.email,
+      username: inboundState.username,
+    };
+  },
+  // Transform function to merge state when loading
+  (outboundState, key) => {
+    return {
+      isUserRegistered: outboundState.isUserRegistered,
+      email: outboundState.email,
+      username: outboundState.username,
+    };
+  },
+  { whitelist: ["user"] } // Apply only to the 'user' slice
+);
+
+// Custom transform to persist some states in userSlice
+const authTransform = createTransform(
+  // Transform function to filter data when saving
+  (inboundState, key) => {
+    return {
+      isUserLoggedIn: inboundState.isUserLoggedIn,
+      email: inboundState.email,
+    };
+  },
+  // Transform function to merge state when loading
+  (outboundState, key) => {
+    return {
+      isUserLoggedIn: outboundState.isUserLoggedIn,
+      email: outboundState.email,
+    };
+  },
+  { whitelist: ["authentication"] } // Apply only to the 'auth' slice
+);
+
 // 1. Configure Persist
 const persistConfig = {
   key: "root",
   storage,
-  whitelist: isSessionActive() ? ["anime", "manga"] : [],
-  blacklist: ["authentication", "user"],
+  whitelist: isSessionActive()
+    ? ["anime", "manga", "authentication", "user"]
+    : [],
+  transforms: [userTransform, authTransform],
   stateReconciler: autoMergeLevel2,
 };
 
