ecdf10e5e4fe0c5098d51f83c1378b741c21768f
Phuc <dongvuminhphuc@gmail.com>
2025-02-09 19:27:19 +0700

    Updated logic to render correct pages

--- src/pages/Visitor.page.js
+++ src/pages/Visitor.page.js
@@ -1,22 +1,31 @@
 import HeroSection from "../components/VisitorPage/HeroSection";
 import BrowseSection from "../components/VisitorPage/BrowseSection";
-import { Outlet, useLocation } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import CustomPaddingLayout from "../components/common/CustomPaddingLayout";
 import AnimeListPage from "./AnimeList.page";
+import { useSelector } from "react-redux";
 
 function VisitorPage() {
   const location = useLocation();
-
-  return (
-    <>
-      {location.pathname === "/" && <HeroSection />}
-      <CustomPaddingLayout>
-        <BrowseSection />
-        {location.pathname === "/" && <AnimeListPage />}
-        <Outlet />
-      </CustomPaddingLayout>
-    </>
+  const isUserLoggedIn = useSelector(
+    (state) => state.authentication.isUserLoggedIn
   );
+  const isUserRegistered = useSelector((state) => state.user.isUserRegistered);
+
+  if ((isUserLoggedIn || isUserRegistered) && location.pathname === "/") {
+    return <Navigate to="/home" />;
+  } else {
+    return (
+      <>
+        {location.pathname === "/" && <HeroSection />}
+        <CustomPaddingLayout>
+          <BrowseSection />
+          {location.pathname === "/" && <AnimeListPage />}
+          <Outlet />
+        </CustomPaddingLayout>
+      </>
+    );
+  }
 }
 
 export default VisitorPage;
