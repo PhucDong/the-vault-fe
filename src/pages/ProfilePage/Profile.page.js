8f7ad4d38c2f421a8fde37c85ed4a92dd0542df3
Phuc <dongvuminhphuc@gmail.com>
2025-02-09 18:49:44 +0700

    Created Profile page

---
+++ src/pages/ProfilePage/Profile.page.js
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+function ProfilePage() {
+  return (
+    <Box>
+      <Typography>This is a profile page.</Typography>
+    </Box>
+  );
+}
+
+export default ProfilePage;
