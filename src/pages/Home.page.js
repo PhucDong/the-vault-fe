f9534bf5af08d2a69edf593a4bc0590aa46af399
Phuc <dongvuminhphuc@gmail.com>
2025-02-09 19:47:46 +0700

    Updated the component

--- src/pages/Home.page.js
+++ src/pages/Home.page.js
@@ -1,11 +1,35 @@
 import { Box, Typography } from "@mui/material";
+import { useAppSelector } from "../services/hooks";
+import { selectIsUserLoggedIn } from "../store/slices/authentication/authenticationSlice";
+import { selectIsUserRegistered } from "../store/slices/user/userSlice";
+import { Navigate } from "react-router-dom";
+import CustomPaddingLayout from "../components/common/CustomPaddingLayout";
+import SearchBar from "../components/common/SearchBar";
+import WriteReviewBar from "../components/HomePage/WriteReviewBar";
+import ReviewList from "../components/HomePage/ReviewList";
 
 function HomePage() {
-  return (
-    <Box>
-      <Typography>Welcome to The Vault</Typography>
-    </Box>
-  );
+  const isUserLoggedIn = useAppSelector(selectIsUserLoggedIn);
+  const isUserRegistered = useAppSelector(selectIsUserRegistered);
+
+  if (isUserLoggedIn || isUserRegistered) {
+    return (
+      <CustomPaddingLayout>
+        <Box
+          sx={{
+            marginTop: { xs: "24px", sm: "32px", md: "52px", lg: "72px" },
+            marginBottom: { xs: "28px", md: "44px", lg: "56px" },
+          }}
+        >
+          <SearchBar />
+          <WriteReviewBar />
+        </Box>
+        <ReviewList />
+      </CustomPaddingLayout>
+    );
+  } else {
+    return <Navigate to="/" />;
+  }
 }
 
 export default HomePage;
