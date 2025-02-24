4ee2be208e47064bc9d3e798c976dd4c720a6da8
Phuc <dongvuminhphuc@gmail.com>
2025-02-24 18:29:33 +0700

    Added updateCommentReactions action

--- src/store/slices/review/reviewSlice.js
+++ src/store/slices/review/reviewSlice.js
@@ -9,7 +9,8 @@ const initialState = {
   text: "",
   score: "",
   review: {},
-  reactions: {}, // Store reactions per review
+  reactions: {},
+  comments: {},
 };
 
 export const fetchTitleListBasedOnFormat = createAsyncThunk(
@@ -54,7 +55,7 @@ export const reviewSlice = createSlice({
     updateReview(state, action) {
       state.review = action.payload;
     },
-    updateReactions(state, action) {
+    updateReviewReactions(state, action) {
       const { reviewId, emoji } = action.payload;
 
       if (!state.reactions[reviewId]) {
@@ -68,6 +69,31 @@ export const reviewSlice = createSlice({
         state.reactions[reviewId].isDisliked =
           !state.reactions[reviewId].isDisliked;
         state.reactions[reviewId].isLiked = false;
+      }
+    },
+    updateCommentReactions(state, action) {
+      const { commentId, emoji } = action.payload;
+
+      if (!state.reactions[commentId]) {
+        state.reactions[commentId] = { isLiked: false, isDisliked: false };
+      }
+
+      if (emoji === "like") {
+        state.reactions[commentId].isLiked = !state.reactions[commentId].isLiked;
+        state.reactions[commentId].isDisliked = false;
+      } else {
+        state.reactions[commentId].isDisliked =
+          !state.reactions[commentId].isDisliked;
+        state.reactions[commentId].isLiked = false;
+      }
+    },
+    updateComments(state, action) {
+      const { reviewId, comments } = action.payload;
+
+      if (!state.comments[reviewId]) {
+        state.comments[reviewId] = { comments };
+      } else {
+        state.comments[reviewId].comments = [...comments];
       }
     },
     resetReviewState: (state) => {
@@ -96,7 +122,9 @@ export const {
   updateScore,
   updateText,
   updateReview,
-  updateReactions,
+  updateReviewReactions,
+  updateCommentReactions,
+  updateComments,
   resetReviewState,
 } = reviewSlice.actions;
 export default reviewSlice.reducer;
